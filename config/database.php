f65a19a2333d491d731f9d5779977e91763ebd6a
iFeerx <fernando_akjs@hotmail.com>
2024-11-27 12:21:35 -0700

    Feat: Add DB_READ_HOST and DB_WRITE_HOST to .env.testing and configure read/write hosts in database.php

--- config/database.php
+++ config/database.php
@@ -44,6 +44,18 @@ return [
         ],
 
         'mysql' => [
+
+            'read' => [
+                'host' => [
+                    env('DB_READ_HOST', '192.168.1.104'),
+                ],
+            ],
+            'write' => [
+                'host' => [
+                    env('DB_WRITE_HOST', '192.168.1.227'),
+                ],
+            ],
+
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
@@ -125,7 +137,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
